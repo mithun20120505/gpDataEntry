7599c0cd90e5baca406b72edc9e30739f82305c7
mithun20120505 <mithunsahu12btech@gmail.com>
2024-09-14 14:04:59 +0530

    set the db connection throgh .nev

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/gramPanchayatDB', {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
